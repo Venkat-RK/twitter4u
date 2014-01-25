d29afa500121d2af3fc7068f32450722aca538fa
venkateswara.rao <venkateswara.rao@revenuemed.com>
2014-01-25 17:28:59 +0530

    twitter4u initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter4u::Application.config.secret_token = '4098ce8d56b626f3242621553ec108ad3f767c6b4bb5e7ff97c596bf2d770c9ae2e20da3134c3dbde4e7a9c2b6ab55bb3ae55ef8ec273acfbe588c18704689f8'
